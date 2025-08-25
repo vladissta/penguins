3c32e80dd5a40c16762604b624c059ce703a04c1
vladissta <staninvladislav@mail.ru>
2025-08-25 14:28:26 +0300

    hist png added

--- penguins.R
+++ penguins.R
@@ -4,3 +4,8 @@ x = data$bill_length_mm
 hist(x, breaks = seq(40, 60, 4))
 hist(x, breaks = seq(40, 60, 2))
 hist(x, breaks = seq(40, 60, 1))
+
+
+png("penguins-hist.png")
+hist(x, breaks = seq(40, 60, 2))
+dev.off()

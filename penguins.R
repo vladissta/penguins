f4aa8e6c199b7eb0c2c379f27dce3713606e0713
vladissta <staninvladislav@mail.ru>
2025-08-25 14:34:28 +0300

    boxplot & del csv

--- penguins.R
+++ penguins.R
@@ -1,11 +1,6 @@
 data <- read.csv("penguins.csv")
-x = data$bill_length_mm
+x <- data$bill_length_mm
 
-hist(x, breaks = seq(40, 60, 4))
-hist(x, breaks = seq(40, 60, 2))
-hist(x, breaks = seq(40, 60, 1))
-
-
-png("penguins-hist.png")
-hist(x, breaks = seq(40, 60, 2))
+png("penguins-boxplot.png")
+boxplot(x)
 dev.off()
